9416f360191b8d1a7c5044feed0b0d4edc3c4dd3
Luis Gabriel] <luisgabrielalencar669@gmail.com>
2026-07-04 12:56:08 -0300

    Defining ADT of Binary_search_tree

--- Data Structure/Binary_search_tree.h
+++ Data Structure/Binary_search_tree.h
@@ -5,4 +5,28 @@
 #ifndef INTRODUCTION_TO_ALGORITHMS_IMPLEMENTATION_BINARY_SEARCH_TREE_H
 #define INTRODUCTION_TO_ALGORITHMS_IMPLEMENTATION_BINARY_SEARCH_TREE_H
 
+typedef struct node
+{
+    struct node *left, *right;
+    int key;
+    unsigned char value; // 1 byte
+} node_t;
+
+typedef struct tree
+{
+    struct node *root;
+} tree_t;
+
+node_t *node_t create_node(int key, unsigned char value);
+node_t *search_node(tree_t *tree, int key);
+node_t tree_minimum(node_t *x);
+node_t tree_maximum(node_t *x);
+
+tree_t *create_tree();
+
+void insert_node(tree_t *tree, node_t *x);
+void delete_node(tree_t *tree, node_t *x);
+void inorder_tree_walk(node_t *root);
+
+
 #endif //INTRODUCTION_TO_ALGORITHMS_IMPLEMENTATION_BINARY_SEARCH_TREE_H
